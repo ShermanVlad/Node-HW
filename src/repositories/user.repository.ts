837db91a6bb5efee6897035cea6b74d0a4955351
ShermanVlad <vladsheremeta5@gmail.com>
2024-07-18 23:23:02 +0300

    hw4

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -23,6 +23,44 @@ class UserRepository {
     await fsService.write(users);
     return newUser;
   }
+
+  public async getById(userId: number): Promise<IUser> {
+    const users = await fsService.read();
+    const user = users.find((user) => user.id === userId);
+    if (!user) {
+      throw new ApiError("User not found", 422);
+    }
+
+    return user;
+  }
+
+  public async updateById(userId: number, dto: IUser): Promise<IUser> {
+    const users = await fsService.read();
+    const user = users.find((user) => user.id === userId);
+
+    if (!user) {
+      throw new ApiError("User not found", 422);
+    }
+
+    if (dto.password) user.password = dto.password;
+    if (dto.name) user.name = dto.name;
+    if (dto.email) user.email = dto.email;
+
+    await fsService.write(users);
+    return user;
+  }
+
+  public async deleteById(userId: number): Promise<void> {
+    const users = await fsService.read();
+    const userIndex = users.findIndex((user) => user.id === userId);
+
+    if (userIndex === -1) {
+      throw new ApiError("User not found", 422);
+    }
+
+    users.splice(userIndex, 1);
+    await fsService.write(users);
+  }
 }
 
 export const userRepository = new UserRepository();
